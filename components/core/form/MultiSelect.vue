7fb81f11f0fca8250489c08a7fd58bb5b336833a
kyle <mr.th2.90@gmail.com>
2021-12-16 23:00:45 +0700

    update multiselect style

--- components/core/form/MultiSelect.vue
+++ components/core/form/MultiSelect.vue
@@ -2,59 +2,68 @@
 import Vue from 'vue'
 import Multiselect from 'vue-multiselect'
 export default Vue.extend({
-  name: 'MultiSelect',
-  components: {
-    Multiselect
-  },
-  inheritAttrs: false,
-  // eslint-disable-next-line vue/require-prop-types
-  props: ['model'],
-  computed: {
-    propModel: {
-      get(): any {
-        return this.model
-      },
-      set(value: any): void {
-        this.$emit('update:model', value)
-      }
-    }
-  }
+	name: 'MultiSelect',
+	components: {
+		Multiselect
+	},
+	inheritAttrs: false,
+	// eslint-disable-next-line vue/require-prop-types
+	props: ['model'],
+	computed: {
+		propModel: {
+			get(): any {
+				return this.model
+			},
+			set(value: any): void {
+				this.$emit('update:model', value)
+			}
+		}
+	}
 })
 </script>
 <template>
-  <Multiselect v-model="propModel" class="form-control" v-bind="$attrs" v-on="$listeners" />
+	<Multiselect
+		v-model="propModel"
+		class="form-control"
+		v-bind="$attrs"
+		select-label=''
+    v-on="$listeners"
+	/>
 </template>
 <style scoped lang="postcss">
 .multiselect ::v-deep {
-  @apply py-0 w-auto;
-  min-height: 40px;
-  .multiselect__spinner {
-    @apply bg-transparent;
-  }
-  .multiselect__tags {
-    @apply border-0 border-none bg-transparent pb-0 pl-0 h-auto min-h-0;
-  }
-  .multiselect__tag {
-    @apply bg-tag text-black py-2.5;
-    .multiselect__tag-icon {
-      @apply hover:bg-transparent;
+	@apply py-0 w-auto;
+	min-height: 40px;
+	.multiselect__spinner {
+		@apply bg-transparent;
+	}
+	.multiselect__tags {
+		@apply border-0 border-none bg-transparent pb-0 pl-0 h-auto min-h-0;
+	}
+	.multiselect__tag {
+		@apply bg-tag text-black py-2.5;
+		.multiselect__tag-icon {
+			@apply hover:bg-transparent;
 
-      &:after {
-        @apply text-black;
-      }
-    }
-  }
-  .multiselect__tag-icon {
-    @apply pt-1.5;
-  }
-  .multiselect__input {
-    @apply bg-transparent;
-  }
-  .multiselect__single {
-    @apply leading-6;
-  }
-  .multiselect__placeholder {
-    @apply pt-0 mb-2;
-  }
+			&:after {
+				@apply text-black;
+			}
+		}
+	}
+	.multiselect__tag-icon {
+		@apply pt-1.5;
+	}
+	.multiselect__input {
+		@apply bg-transparent;
+	}
+	.multiselect__single {
+		@apply leading-6;
+	}
+	.multiselect__placeholder {
+		@apply pt-0 mb-2;
+	}
+	.multiselect__option--highlight {
+		background: #bfbfc2;
+	}
 }
 </style>
